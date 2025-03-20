3c8bd95acb06dc5150cf57454e5b5eccab345226
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-03-20 00:32:40 -0400

    add login and register pages

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Page() {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center bg-background">
+      <div className="w-full max-w-md overflow-hidden rounded-2xl gap-12 flex flex-col">
+        <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
+          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign Up</h3>
+          <p className="text-sm text-gray-500 dark:text-zinc-400">
+            Create an account with your email and password
+          </p>
+        </div>
+      </div>
+    </div>
+    );
+}
