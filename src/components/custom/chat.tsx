d05f26cd0134bfc225257275f8ffe8504055671a
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-03-31 21:13:17 -0400

    add textarea element for input in chat page

--- src/components/custom/chat.tsx
+++ src/components/custom/chat.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+import { ArrowUpIcon } from "./icons";
 // import { Attachment, Message } from "ai";
 // import { useChat } from "ai/react";
 // import { useState } from "react";
@@ -62,8 +65,8 @@ console.log(id, initialMessages);
           />
         </div>
 
-        {/* <form className="flex flex-row gap-2 relative items-end w-full md:max-w-[500px] max-w-[calc(100dvw-32px) px-4 md:px-0">
-          <MultimodalInput
+        <form className="flex flex-row gap-2 relative items-end w-full md:max-w-[500px] max-w-[calc(100dvw-32px) px-4 md:px-0">
+          {/* <MultimodalInput
             input={input}
             setInput={setInput}
             handleSubmit={handleSubmit}
@@ -73,8 +76,35 @@ console.log(id, initialMessages);
             setAttachments={setAttachments}
             messages={messages}
             append={append}
-          />
-        </form> */}
+          /> */}
+          <div className="relative w-full flex flex-col gap-4">
+          <Textarea
+            // ref={textareaRef}
+            placeholder="Send a message..."
+            value={'test input'}
+            onChange={() => console.log("change")}
+            className="min-h-[24px] overflow-hidden resize-none rounded-lg text-base bg-muted border-none"
+            rows={3}
+            onKeyDown={(event: any) => {
+            if (event.key === "Enter" && !event.shiftKey) {
+                event.preventDefault();
+
+                console.log(event);
+            }
+            }}
+            />
+            <Button
+                className="rounded-full p-1.5 h-fit absolute bottom-2 right-2 m-0.5 text-white"
+                onClick={(event: any) => {
+                    event.preventDefault();
+                    console.log(event);
+                }}
+                // disabled={input.length === 0 || uploadQueue.length > 0}
+                >
+                <ArrowUpIcon size={14} />
+            </Button>    
+          </div>
+        </form>
       </div>
     </div>
   );
